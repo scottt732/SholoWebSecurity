548c4e4587053ecd96eb45ff9ff66298d486a991
Scott Holodak <scottt732@gmail.com>
2012-05-10 22:50:53 -0400

    Added test console app

---
+++ Sholo.Web.Security.ConfigTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sholo.Web.Security.ConfigTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UserAuthentication.Enabled);
+            Console.ReadLine();
+        }
+    }
+}
